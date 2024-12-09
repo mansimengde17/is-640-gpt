91e8e7e1ba39dee727c97a0ff6b2dc37b8278bd0
Mansi Ganesh Mengde <126674659+mansimengde17@users.noreply.github.com>
2024-12-08 18:15:06 -0800

    Add files via upload
    This file defines the "Data" class responsible for encoding and decoding text data. It includes functions to read data from a specified file, preprocess it for training, and convert text into numerical tokens for model input. The class also handles the reverse operation, converting model predictions back into human-readable text.

---
+++ data.py
@@ -0,0 +1,33 @@
+import torch
+
+class Data:
+    def __init__(self, file_path, device='cpu', block_size=256):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            self.text = f.read()
+
+        # create character-level encoding
+        self.chars = sorted(list(set(self.text)))
+        self.vocab_size = len(self.chars)
+        self.stoi = { ch:i for i,ch in enumerate(self.chars) }
+        self.itos = { i:ch for i,ch in enumerate(self.chars) }
+        
+        self.encode = lambda s: [self.stoi[c] for c in s]
+        self.decode = lambda l: ''.join([self.itos[i] for i in l])
+
+        self.data = torch.tensor(self.encode(self.text), dtype=torch.long)
+        self.block_size = block_size
+        self.device = device
+
+    def split_data(self, split_ratio=0.9):
+        n = int(split_ratio * len(self.data))
+        train_data = self.data[:n]
+        val_data = self.data[n:]
+        return train_data, val_data
+
+    def get_batch(self, split, batch_size, train_data, val_data):
+        data = train_data if split == 'train' else val_data
+        ix = torch.randint(len(data) - self.block_size, (batch_size,))
+        x = torch.stack([data[i:i+self.block_size] for i in ix])
+        y = torch.stack([data[i+1:i+self.block_size+1] for i in ix])
+        x, y = x.to(self.device), y.to(self.device)
+        return x, y
